4d8e804377f0a638d7e683fc81d30155c707d043
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2021-01-31 11:47:51 -0300

    Animación y otros

--- Game/Camera/Camera.gd
+++ Game/Camera/Camera.gd
@@ -2,6 +2,10 @@ extends Camera2D
 
 
 func _ready():
+	update_cam_limits()
+	
+	
+func update_cam_limits():
 	limit_left = $Pos1.global_position.x
 	limit_top = $Pos1.global_position.y
 	
--- Game/Levels/Level_3/Level_3.gd
+++ Game/Levels/Level_3/Level_3.gd
@@ -1,2 +1,13 @@
 extends Node2D
 
+
+func _on_AnimZoom_body_entered(body):
+	$Player/Camera/Pos1.global_position = $NewPos/Pos1.global_position
+	$Player/Camera/Pos2.global_position = $NewPos/Pos2.global_position
+	
+	yield(get_tree().create_timer(3.0), "timeout")
+	$Text2.hide()
+	
+	$Player/Camera.update_cam_limits()
+	
+	$AnimZoom/Anim.play("ZoomPlay")
