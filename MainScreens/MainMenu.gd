f8a05d1892c0afdd3429e9922cd74a50050886da
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2021-01-31 12:44:49 -0300

    MiniCambios

--- MainScreens/MainMenu.gd
+++ MainScreens/MainMenu.gd
@@ -5,10 +5,13 @@ func _ready():
 
 func _on_Start_body_entered(body):
 	if body == $Player:
-#		get_tree().change_scene("res://Tests/GameplayTest.tscn")
 		Transition.change_scene("res://Game/Levels/Level_1/Level_1.tscn")
 
 
 func _on_Exit_body_entered(body):
 	if body == $Player:
 		get_tree().quit()
+
+
+func _on_Credits_pressed():
+	Transition.change_scene("res://MainScreens/Credits.tscn")
