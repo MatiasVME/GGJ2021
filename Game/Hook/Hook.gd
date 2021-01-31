a1d6dce81f30a4c173e2fd5b044afe4cefcc0d2f
Marcelo Negrier <60198827+StrellReirgen@users.noreply.github.com>
2021-01-31 13:50:23 -0300

    algo

--- Game/Hook/Hook.gd
+++ Game/Hook/Hook.gd
@@ -5,7 +5,7 @@ func _on_Area_body_entered(body):
 	var tween = get_node("Tween")
 	SoundManager.play_sound("CANDADO_ENGANCHE")
 	tween.interpolate_property(body, "position",
-		Vector2(body.position.x, body.position.y), Vector2( self.position.x, self.position.y + 40), 0.2,
+		Vector2(body.position.x, body.position.y), Vector2( self.position.x + 20, self.position.y + 65), 0.2,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
 
--- Game/Levels/Level_4/Level_4.gd
+++ Game/Levels/Level_4/Level_4.gd
@@ -1,5 +1,4 @@
 extends Node2D
 
 func _ready():
-	$Respawn.set_player($Player)
 	MusicManager.play(MusicManager.Music.NIVEL_3)
